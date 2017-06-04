168b392a76205027802a6c75595d64f52177e5d0
Hadi <rahmatkhah@gmail.com>
2017-06-04 19:03:46 -0400

    Update the project 1

--- 03 - Exploratory Data Analysis/ExData_Plotting1/Project 1.R
+++ 03 - Exploratory Data Analysis/ExData_Plotting1/Project 1.R
@@ -4,13 +4,47 @@ hpc <- read.table(file, header = TRUE, sep = ";",
            na.strings = "?", colClasses = c("character", "character", "numeric",
            "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
-hpc <- transform(hpc, Date = as.Date(Date, format = c("%d/%m/%Y")))
+hpc <- transform(hpc, 
+                 DateTime = as.POSIXct(paste(Date, Time, sep = " "), 
+                                  format = c("%d/%m/%Y %H:%M:%S")))
 
-#hpc <- transform(hpc, 
-#                 DateTime = as.POSIXct(paste(Date, Time, sep = ""), 
-#                                  format = c("%d/%m/%Y %H:%M:%S")))
+# hpc <- transform(hpc, Date = as.Date(Date, format = c("%d/%m/%Y")))
 
-hpcs <- subset(hpc, Date >= "2007-02-01" & Date <= "2007-02-02")
+hpcs <- subset(hpc, DateTime >= "2007-02-01" & DateTime < "2007-02-03")
 
 head(hpcs)
 tail(hpcs)
+
+# Plot 1
+hist(hpcs$Global_active_power, col = "red", 
+     xlab = "Global Active Power (kilowatts)", 
+     main = "Global Active Power")
+
+# Plot 2
+plot(hpcs$DateTime, hpcs$Global_active_power, type = "n",
+     ylab = "Global Active Power (kilowatts)", 
+     xlab = "")
+lines(hpcs$DateTime, hpcs$Global_active_power)
+
+# Plot 3
+plot(hpcs$DateTime, hpcs$Sub_metering_1, type = "n",
+     ylab = "Energy sub metering", 
+     xlab = "")
+lines(hpcs$DateTime, hpcs$Sub_metering_1)
+lines(hpcs$DateTime, hpcs$Sub_metering_2, col = "red")
+lines(hpcs$DateTime, hpcs$Sub_metering_3, col = "blue")
+legend("topright", lty=c(1,1,1),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"))
+
+# Plot 4
+plot(hpcs$DateTime, hpcs$Sub_metering_1, type = "n",
+     ylab = "Energy sub metering", 
+     xlab = "")
+lines(hpcs$DateTime, hpcs$Sub_metering_1)
+lines(hpcs$DateTime, hpcs$Sub_metering_2, col = "red")
+lines(hpcs$DateTime, hpcs$Sub_metering_3, col = "blue")
+legend("topright", lty=c(1,1,1),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"))
+
