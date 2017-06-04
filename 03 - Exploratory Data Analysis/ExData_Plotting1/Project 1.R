b1bec12f28f60d82aeddf8995e4f38f865f8d97b
Hadi <rahmatkhah@gmail.com>
2017-06-04 19:17:01 -0400

    Update the project 1

--- 03 - Exploratory Data Analysis/ExData_Plotting1/Project 1.R
+++ 03 - Exploratory Data Analysis/ExData_Plotting1/Project 1.R
@@ -1,19 +1,13 @@
 file <- "./data/household_power_consumption.txt"
 
 hpc <- read.table(file, header = TRUE, sep = ";",
-           na.strings = "?", colClasses = c("character", "character", "numeric",
-           "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+                  na.strings = "?", colClasses = c("character", "character", "numeric",
+                                                   "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 hpc <- transform(hpc, 
-                 DateTime = as.POSIXct(paste(Date, Time, sep = " "), 
-                                  format = c("%d/%m/%Y %H:%M:%S")))
-
-# hpc <- transform(hpc, Date = as.Date(Date, format = c("%d/%m/%Y")))
-
-hpcs <- subset(hpc, DateTime >= "2007-02-01" & DateTime < "2007-02-03")
-
-head(hpcs)
-tail(hpcs)
+                 datetime = as.POSIXct(paste(Date, Time, sep = " "), 
+                                       format = c("%d/%m/%Y %H:%M:%S")))
+hpcs <- subset(hpc, datetime >= "2007-02-01" & datetime < "2007-02-03")
 
 # Plot 1
 hist(hpcs$Global_active_power, col = "red", 
@@ -21,30 +15,44 @@ hist(hpcs$Global_active_power, col = "red",
      main = "Global Active Power")
 
 # Plot 2
-plot(hpcs$DateTime, hpcs$Global_active_power, type = "n",
+plot(hpcs$datetime, hpcs$Global_active_power, type = "n",
      ylab = "Global Active Power (kilowatts)", 
      xlab = "")
-lines(hpcs$DateTime, hpcs$Global_active_power)
+lines(hpcs$datetime, hpcs$Global_active_power)
 
 # Plot 3
-plot(hpcs$DateTime, hpcs$Sub_metering_1, type = "n",
+plot(hpcs$datetime, hpcs$Sub_metering_1, type = "n",
      ylab = "Energy sub metering", 
      xlab = "")
-lines(hpcs$DateTime, hpcs$Sub_metering_1)
-lines(hpcs$DateTime, hpcs$Sub_metering_2, col = "red")
-lines(hpcs$DateTime, hpcs$Sub_metering_3, col = "blue")
+lines(hpcs$datetime, hpcs$Sub_metering_1)
+lines(hpcs$datetime, hpcs$Sub_metering_2, col = "red")
+lines(hpcs$datetime, hpcs$Sub_metering_3, col = "blue")
 legend("topright", lty=c(1,1,1),
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col = c("black", "red", "blue"))
 
 # Plot 4
-plot(hpcs$DateTime, hpcs$Sub_metering_1, type = "n",
+par(mfrow = c(2, 2))
+
+plot(hpcs$datetime, hpcs$Global_active_power, type = "n",
+     ylab = "Global Active Power", 
+     xlab = "")
+lines(hpcs$datetime, hpcs$Global_active_power)
+
+plot(hpcs$datetime, hpcs$Voltage, type = "n",
+     ylab = "Voltage", 
+     xlab = "")
+lines(hpcs$datetime, hpcs$Voltage)
+
+plot(hpcs$datetime, hpcs$Sub_metering_1, type = "n",
      ylab = "Energy sub metering", 
      xlab = "")
-lines(hpcs$DateTime, hpcs$Sub_metering_1)
-lines(hpcs$DateTime, hpcs$Sub_metering_2, col = "red")
-lines(hpcs$DateTime, hpcs$Sub_metering_3, col = "blue")
+lines(hpcs$datetime, hpcs$Sub_metering_1)
+lines(hpcs$datetime, hpcs$Sub_metering_2, col = "red")
+lines(hpcs$datetime, hpcs$Sub_metering_3, col = "blue")
 legend("topright", lty=c(1,1,1),
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col = c("black", "red", "blue"))
 
+with(hpcs, plot(datetime, Global_reactive_power, type = "n"))
+lines(hpcs$datetime, hpcs$Global_reactive_power)
